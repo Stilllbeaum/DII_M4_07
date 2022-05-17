96b3da77e4da0554ecd9b6e0675302c749c826b5
Stilllbeaum <narumol.jeenjuhepan@gmail.com>
2022-05-17 14:14:48 +0700

    fac

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-let netIncome = 999999;
-let tax;
-if(netIncome>5000000){
-    tax=(netIncome-5000000)*0.35+1265000
-}else if (netIncome>2000000){
-    tax= (netIncome-2000000)*0.3+365000
+//for loop
+n = 3;
+fac_for = 1;
+for (let i = 1; i <=n; i++) {
+    fac_for*=(n-1);
 }
-else if (netIncome>1000000){
-    tax=(netIncome-1000000)*0.25+115000
+console.log(fac_for)
+
+//wile loop
+n = 6;
+let fac_while = 1;
+while (n > 0) {
+    fac_while *= n;
+    n--;
 }
-else if (netIncome>750000){
-    tax=(netIncome-750000)*0.2+65000
-}
-else if (netIncome>500000){
-    tax=(netIncome-500000)*0.15+27000
-}
-else if (netIncome>30000){
-    tax=(netIncome-30000)*0.1+7500
-}
-else if (netIncome<=150000){
-    tax=(netIncome-150000)*0.05
-}
-else{
-    tax=0
-}
-console.log('You have to pay ' + tax)
+console.log(sum2)
