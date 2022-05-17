cce2bfcb94c9e666d048b03a21747cee64ccc66b
Stilllbeaum <narumol.jeenjuhepan@gmail.com>
2022-05-17 13:28:03 +0700

    grade

---
+++ script.js
@@ -0,0 +1,12 @@
+let score = 79.4;
+let grade;
+if (score >= 80) {
+ grade = 'A';
+} else if (score >= 75) {
+ grade = 'B+';
+} else if (score >= 70) {
+ grade = 'B';
+} else if (score >= 65) {
+ grade = 'C+';
+}
+console.log(grade + ' ' + score)
