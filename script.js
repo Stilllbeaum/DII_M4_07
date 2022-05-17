97399ff73075a79b34ea5c3731f0767f36729479
Stilllbeaum <narumol.jeenjuhepan@gmail.com>
2022-05-17 13:47:26 +0700

    tax

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let score = 0;
-let grade;
-if (score >= 80) {
- grade = 'A';
-} else if (score >= 75) {
- grade = 'B+';
-} else if (score >= 70) {
- grade = 'B';
-} else if (score >= 65) {
- grade = 'C+';
+let netIncome = 999999;
+let tax;
+if(netIncome>5000000){
+    tax=(netIncome-5000000)*0.35+1265000
+}else if (netIncome>2000000){
+    tax= (netIncome-2000000)*0.3+365000
 }
-console.log(grade + ' ' + score)
+else if (netIncome>1000000){
+    tax=(netIncome-1000000)*0.25+115000
+}
+else if (netIncome>750000){
+    tax=(netIncome-750000)*0.2+65000
+}
+else if (netIncome>500000){
+    tax=(netIncome-500000)*0.15+27000
+}
+else if (netIncome>30000){
+    tax=(netIncome-30000)*0.1+7500
+}
+else if (netIncome<=150000){
+    tax=(netIncome-150000)*0.05
+}
+else{
+    tax=0
+}
+console.log('You have to pay ' + tax)
